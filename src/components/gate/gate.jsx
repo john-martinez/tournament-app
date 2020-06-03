6808210270f8cc25e9dfd9886ff5a86c234cfe8e
John Ray Martinez <jrvmartinez@icloud.com>
2020-06-03 18:52:27 -0400

    implemented gate for cancelled tournaments and new

---
+++ src/components/gate/gate.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function Gate({
+  type,
+  handler,
+}){
+
+  const isPaused = type === 'paused';
+  const isNew = type === 'new';
+  const isCancelled = type === 'canceled';
+
+  const generateCopy = () => {
+    switch (true){
+      case isPaused: return "Tournament is currently paused";
+      case isNew: return "Let's get this tournament started!";
+      case isCancelled: return "Tournament is cancelled.";
+      default: // do nothing
+    }
+  }
+  return(
+    <div className="tournament-details__modal">
+      <h2 className="tournament-details__copy">
+        {generateCopy()}
+      </h2>
+      {!isCancelled && (
+        <button className="tournament-details__cta" onClick={handler}>{isNew ? 'START' : 'RESUME'}</button>
+      )}
+    </div>
+  );
+}
--- src/pages/tournamentDetails/tournamentDetails.jsx
+++ src/pages/tournamentDetails/tournamentDetails.jsx
@@ -1,5 +1,6 @@
 import React, { Component }  from 'react';
 import Round from '../../components/round/round';
+import Gate from '../../components/gate/gate';
 import './tournamentDetails.scss';
 import axios from 'axios';
 
@@ -40,22 +41,33 @@ export default class TournamentDetails extends Component {
     }
     return rounds;
   }
+
+  startTournament = () => {
+    const { id } = this.props.match.params
+    axios.put(`http://localhost:8000/tournament/${id}/start`, {})
+      .then(_=>this.retrieveTournament())
+      .catch(err=>console.log(err))
+  }
   
+  
+  renderCopy = () => {
+    const { status } = this.state.tournament;
+    return <Gate type={ status } handler={this.startTournament} />
+  }
   render(){
     const { rounds, tournament } = this.state;
     const { status } = tournament;
     const doesRoundsExist = !!rounds[1];
-    const isGated = !!(status === 'paused' || status === 'new')
-
+    const isGated = !!(status === 'paused' || status === 'new' || status === 'canceled')
+    console.log(tournament);
     return(
       <div className="tournament-details">
         {doesRoundsExist && this.renderRoundList()}
         { isGated && (
           <div className="tournament-details__gate">
-            <h2 className="tournament-details__copy">Tournament is currently {status}</h2>
+            {this.renderCopy()}
           </div>
         )}
-        
       </div>
     );
   }
